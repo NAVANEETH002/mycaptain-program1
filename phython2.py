c8e933c127dd0fa737df7c595904e41c3935a8eb
NAVANEETH002 <70953792+NAVANEETH002@users.noreply.github.com>
2020-12-19 21:33:35 +0800

    Add files via upload

---
+++ phython2.py
@@ -0,0 +1,8 @@
+filename = input("Input the filename :")
+file_ext = filename.split(".")
+print("File name is :",filename);
+print("file extension is :"+repr(file_ext[-1]));
+      
+    
+    
+    
